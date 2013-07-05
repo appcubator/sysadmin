2317dcdaa2d0b194e8cc8e0d87634d61c7e3074a
v1 Factory <founders@v1factory.com>
2013-07-05 03:18:36 +0000

    working

--- setup_stuff.py
+++ setup_stuff.py
@@ -20,38 +20,50 @@ import subprocess
 import shlex
 
 
-this_dir = os.path.abspath(os.path.dirname(__FILE__))
+this_dir = os.path.abspath(os.path.dirname(__file__))
 nginx_dir = '/etc/nginx/sites-enabled'
 upstart_dir = "/etc/init"
 
-staging = os.environ['APPCUBATOR_MODE']
-assert staging in ['prod', 'staging'], "export APPCUBATOR_MODE to staging or prod"
+try:
+    staging = os.environ['APPCUBATOR_MODE']
+    assert staging in ['prod', 'staging']
+except (KeyError, AssertionError):
+    print >> sys.stderr, "export APPCUBATOR_MODE to staging or prod"
+    sys.exit(1)
 
 
 def run_command(command):
     shell_splitted_command = shlex.split(command)
-    p = subprocess.Popen(shell_splitted_command, stdout=sys.STDOUT, stderr=sys.STDERR)
+    p = subprocess.Popen(shell_splitted_command, stdout=sys.stdout, stderr=sys.stderr)
     p.wait()
-    return p.return_code
+    return p.returncode
 
-# nginx
-os.symlink(j(this_dir, 'nginx_configs', 'site', 'nginx-%s.conf' % staging), j(nginx_dir, 'site-nginx-%s.conf' % staging))
-os.symlink(j(this_dir, 'nginx_configs', 'deploy', 'nginx-%s.conf' % staging), j(nginx_dir, 'deploy-nginx-%s.conf' % staging))
+def symlink(src, dest, *args, **kwargs):
+    if os.path.exists(dest):
+        print >> sys.stderr, "About to link %s to %s, overwriting %s, proceed? [y/n]:" % (dest, src, dest)
+        ch = raw_input()
+        if ch.lower().startswith('y'):
+            os.remove(dest)
+        else:
+            return
 
-# uwsgi for the site
-site_uwsgi_ini = j(this_dir, 'site', 'uwsgi-%s.ini' % staging)
-site_upstart_template = open(j(this_dir, 'site', 'upstart-uwsgi.conf'), "r").read()
-site_upstart = site_upstart_template.format(UWSGI_INI_LOCATION='site_uwsgi_ini',
-                                            UWSGI_FOR='appcubator-site')
+    os.symlink(src, dest, *args, **kwargs)
 
-with open(j(upstart_dir, 'uwsgi-site.conf'), "w") as upstart_dest:
-    upstart_dest.write(site_upstart)
+def deploy_upstart(ini_path, desc, command_name):
+    site_upstart_template = open(j(this_dir, 'upstart.conf.template'), "r").read()
+    site_upstart = site_upstart_template.format(UWSGI_INI_LOCATION=ini_path,
+                                                UWSGI_FOR=desc)
 
-# uwsgi for the deployment
-deploy_uwsgi_ini = j(this_dir, 'deploy', 'uwsgi-%s.ini' % staging)
-deploy_upstart_template = open(j(this_dir, 'deploy', 'upstart-uwsgi.conf'), "r").read()
-deploy_upstart = deploy_upstart_template.format(UWSGI_INI_LOCATION='deploy_uwsgi_ini',
-                                            UWSGI_FOR='appcubator-deploy')
+    with open(j(upstart_dir, '%s.conf' % command_name), "w") as upstart_dest:
+        upstart_dest.write(site_upstart)
 
-with open(j(upstart_dir, 'uwsgi-deploy.conf'), "w") as upstart_dest:
-    upstart_dest.write(site_upstart)
+#site
+symlink(j(this_dir, 'site', 'nginx-%s.conf' % staging), j(nginx_dir, 'site-nginx-%s.conf' % staging))
+deploy_upstart(j(this_dir, 'site', 'uwsgi-%s.ini' % staging), 'appcubator-site', 'uwsgi-site')
+
+#deployment
+symlink(j(this_dir, 'deploy', 'nginx-%s.conf' % staging), j(nginx_dir, 'deploy-nginx-%s.conf' % staging))
+deploy_upstart(j(this_dir, 'deploy', 'uwsgi-deploy.ini'), 'appcubator-deploy', 'uwsgi-deploy')
+deploy_upstart(j(this_dir, 'deploy', 'uwsgi-emperor.ini'), 'appcubator hosting', 'uwsgi-hosting')
+
+run_command('initctl reload-configuration')
