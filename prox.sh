2c90f23c6f1ccd610f928b1161e858d3cdc53300
CoreOS Admin <core@ip-10-141-163-215.ec2.internal>
2014-03-19 19:00:59 +0000

    test

--- prox.sh
+++ prox.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
-# Usage: ./proxy.sh molten-yearling 34532 
+# Usage: ./proxy.sh molten-yearling
 
 REDIS_HOST=10.204.203.78
 SUBDOMAIN=$1
 IP=$(ifconfig eth0 | sed -n 2p | cut -d ":" -f2 | cut -d " " -f10)
-PORT=$2
+echo "docker port output:" 
+/usr/bin/docker port devmon-$SUBDOMAIN 5000
+PORT=$(sleep 3s && /usr/bin/docker port devmon-$SUBDOMAIN 5000 | sed 's/.*:\([:digit:]*\)/\1/')
+echo "port result"
+echo $PORT
 
 curl "http://$REDIS_HOST:7379/DEL/frontend:$SUBDOMAIN.appcbtr.com"
 curl "http://$REDIS_HOST:7379/RPUSH/frontend:$SUBDOMAIN.appcbtr.com/$SUBDOMAIN"
