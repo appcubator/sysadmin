0d38c13a9ee98e9aa9bc25e8489b4413dceeaa41
CoreOS Admin <core@ip-10-170-88-173.ec2.internal>
2014-03-19 19:07:30 +0000

    go to sleep

--- prox.sh
+++ prox.sh
@@ -5,6 +5,7 @@ set -e
 REDIS_HOST=10.204.203.78
 SUBDOMAIN=$1
 IP=$(ifconfig eth0 | sed -n 2p | cut -d ":" -f2 | cut -d " " -f10)
+sleep 20s
 echo "docker port output:" 
 /usr/bin/docker port devmon-$SUBDOMAIN 5000
 PORT=$(sleep 3s && /usr/bin/docker port devmon-$SUBDOMAIN 5000 | sed 's/.*:\([:digit:]*\)/\1/')
