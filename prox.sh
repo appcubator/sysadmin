288db2fc40380b1eeb04c5cee541f5bdd9f62295
CoreOS Admin <core@ip-10-141-163-215.ec2.internal>
2014-03-19 07:57:51 +0000

    wildcard added

--- prox.sh
+++ prox.sh
@@ -8,6 +8,9 @@ IP=$(ifconfig eth0 | sed -n 2p | cut -d ":" -f2 | cut -d " " -f10)
 PORT=$2
 
 curl "http://$REDIS_HOST:7379/DEL/frontend:$SUBDOMAIN.appcbtr.com"
-curl "http://$REDIS_HOST:7379/RPUSH/frontend:$SUBDOMAIN.appcbtr.com/$ID"
+curl "http://$REDIS_HOST:7379/RPUSH/frontend:$SUBDOMAIN.appcbtr.com/$SUBDOMAIN"
 curl "http://$REDIS_HOST:7379/RPUSH/frontend:$SUBDOMAIN.appcbtr.com/http%3A%2F%2F$IP:$PORT"
+
+curl "http://$REDIS_HOST:7379/DEL/frontend:*.$SUBDOMAIN.appcbtr.com"
+curl "http://$REDIS_HOST:7379/RPUSH/frontend:*.$SUBDOMAIN.appcbtr.com/$SUBDOMAIN"
 curl "http://$REDIS_HOST:7379/RPUSH/frontend:*.$SUBDOMAIN.appcbtr.com/http%3A%2F%2F$IP:$PORT"
