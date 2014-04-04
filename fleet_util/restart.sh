28e66482ff8108b076b4ee9966b841bac8e7c753
Karan Sikka <kssworld93@gmail.com>
2014-04-04 19:18:49 -0400

    trying

--- fleet_util/restart.sh
+++ fleet_util/restart.sh
@@ -1,3 +1,4 @@
+set -e
 fleetctl stop $1
 sleep 3
 fleetctl start $1
---
+++ fleet_util/submit_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# TEMP DIRECTORY CODE FROM http://stackoverflow.com/questions/2472629/temporary-operation-in-a-temporary-directory-in-shell-script
+TDIR=`mktemp -d`
+trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
+cp hostnames.txt $TDIR
+cd $TDIR
+
+
+
+# do main stuff here
+for subdomain in $(cat hostnames.txt)
+do
+    cmd = echo $1 | sed "s/SUBDOMAIN/$subdomain"
+    echo $cmd
+done
+# end main stuff
+
+
+
+# cleanup temp directory
+cd -
+rm -rf $TDIR
+
