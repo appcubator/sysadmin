26c1ce5ae42e6a25435319e135fb22e806474aea
Karan Sikka <kssworld93@gmail.com>
2014-04-04 19:40:23 -0400

    oops

--- fleet_util/submit_all.sh
+++ fleet_util/submit_all.sh
@@ -1,1 +1,1 @@
-cd ~/appcubator-sysadmin/fleet_util && ./submit_all.sh "cp ./devmon.service ./devmon@SUBDOMAIN.service && fleetctl submit devmon@SUBDOMAIN.service"
+cd ~/appcubator-sysadmin/fleet_util && ./map_subdomain.sh "cp ./devmon.service ./devmon@SUBDOMAIN.service && fleetctl submit devmon@SUBDOMAIN.service"
