ff82ce2d9bf36aeca840b17d9405a80012267c4e
CoreOS Admin <core@domU-12-31-39-04-2D-C2.compute-1.internal>
2014-04-05 23:53:53 +0000

    polling

--- prox.sh
+++ prox.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 set -e
+set -x
 # Usage: ./proxy.sh molten-yearling
 
 REDIS_HOST=10.204.203.78
 SUBDOMAIN=$1
 IP=$(ifconfig eth0 | sed -n 2p | cut -d ":" -f2 | cut -d " " -f10)
-sleep 10s
+
+sleep 3
+
+echo "Polling docker for the port"
+COUNTER=1
+PORT_OUT=''
+while [ 1 ]; do
 echo "docker port output:" 
-/usr/bin/docker port devmon-$SUBDOMAIN 5000
-PORT=$(sleep 3s && /usr/bin/docker port devmon-$SUBDOMAIN 5000 | sed 's/.*:\([:digit:]*\)/\1/')
+    echo The counter is $COUNTER
+
+PORT_OUT=$(/usr/bin/docker port devmon-$SUBDOMAIN 5000)
+
+
+echo $PORT_OUT
+PORT=$(echo $PORT_OUT | sed 's/.*:\([:digit:]*\)/\1/')
 echo "port result"
 echo $PORT
+if [[ "$PORT" -ne "" ]];
+then
+break
+fi
+    sleep 1
+    let COUNTER=COUNTER+1
+
+done
 
 curl "http://$REDIS_HOST:7379/DEL/frontend:$SUBDOMAIN.appcbtr.com"
 curl "http://$REDIS_HOST:7379/RPUSH/frontend:$SUBDOMAIN.appcbtr.com/$SUBDOMAIN"
